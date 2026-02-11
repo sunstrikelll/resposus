5df71d829e7d1e1f15870a2063225800dab00e0f
sunstrikelll <sunstrike20021@gmail.com>
2026-02-12 02:13:43 +0300

    fix defines

--- include/PWM/PWM.h
+++ include/PWM/PWM.h
@@ -4,10 +4,17 @@
 #include <stdint.h>
 #include "..\..\GD_libraries\CMSIS\DeviceSupport\gd32f10x.h"
 
-#define PWM_PORT        GPIOA
-#define PWM_PIN         GPIO_PIN_0
-#define PWM_TIMER       TIMER2
-#define PWM_CHANNEL     TIMER_CH_1
+#define PWM_PORT           GPIOA
+#define PWM_PIN            GPIO_PIN_0
+#define PWM_FREQ           GPIO_OSPEED_50MHZ
+#define PWM_MODE           GPIO_MODE_AF_PP
+#define PWM_TIMER          TIMER4
+#define PWM_CHANNEL        TIMER_CH_0
+#define PROCPERIOD         1000
+#define PROCMGFREQ         72
+#define GPIO_CLK           RCU_GPIOA
+#define TIMER_GPIO_CLK     RCU_TIMER4
+#define FOR_TO_PRECENT     100
 
 void pwm_init(void);
 
--- src/PWM/PWM.c
+++ src/PWM/PWM.c
@@ -1,20 +1,13 @@
 #include "..\include\PWM\PWM.h"
 
-#define PROCPERIOD         1000
-#define PROCMGFREQ         72
-#define GPIO_CLK           RCU_GPIOA
-#define TIMER_GPIO_CLK     RCU_TIMER4
-#define FOR_TO_PRECENT     100
-
-
 void pwm_init(void) 
 {
 
     rcu_periph_clock_enable(GPIO_CLK);
-    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_0);
+    gpio_init(PWM_PORT, PWM_MODE, PWM_FREQ, PWM_PIN);
     rcu_periph_clock_enable(TIMER_GPIO_CLK);
 
-    timer_deinit(TIMER4);
+    timer_deinit(PWM_TIMER);
     timer_oc_parameter_struct timer_ocinitpara;
     timer_parameter_struct timer_initpara;
 
@@ -24,7 +17,7 @@ void pwm_init(void)
     timer_initpara.period            = PROCPERIOD - 1;
     timer_initpara.clockdivision     = TIMER_CKDIV_DIV1;
     timer_initpara.repetitioncounter = 0;
-    timer_init(TIMER4, &timer_initpara);
+    timer_init(PWM_TIMER, &timer_initpara);
 
     timer_channel_output_struct_para_init(&timer_ocinitpara);
     timer_ocinitpara.outputstate  = TIMER_CCX_ENABLE;
@@ -34,20 +27,21 @@ void pwm_init(void)
     timer_ocinitpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;
     timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
     
-    timer_channel_output_config(TIMER4, TIMER_CH_0, &timer_ocinitpara);
+    timer_channel_output_config(PWM_TIMER, PWM_CHANNEL, &timer_ocinitpara);
 
-    timer_channel_output_mode_config(TIMER4, TIMER_CH_0, TIMER_OC_MODE_PWM0);
-    timer_channel_output_shadow_config(TIMER4, TIMER_CH_0, TIMER_OC_SHADOW_ENABLE);
+    timer_channel_output_mode_config(PWM_TIMER, PWM_CHANNEL, TIMER_OC_MODE_PWM0);
+    timer_channel_output_shadow_config(PWM_TIMER, PWM_CHANNEL, TIMER_OC_SHADOW_ENABLE);
 
-    timer_channel_output_pulse_value_config(TIMER4, PWM_CHANNEL, 0);
+    timer_channel_output_pulse_value_config(PWM_TIMER, PWM_CHANNEL, 0);
 
-    timer_enable(TIMER4);
+    timer_auto_reload_shadow_enable(PWM_TIMER);
+
+    timer_enable(PWM_TIMER);
 }
 
 void pwm_setVoltage(uint8_t percent) 
 {
-    if (percent < 0) percent = 0;
     if (percent > 100) percent = 100;
     uint32_t pulse = (percent * PROCPERIOD) / FOR_TO_PRECENT;
-    timer_channel_output_pulse_value_config(TIMER4, TIMER_CH_0, pulse);
+    timer_channel_output_pulse_value_config(PWM_TIMER, TIMER_CH_0, pulse);
 }
