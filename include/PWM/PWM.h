8ea750792aaa297d882572763e26abb0292550e0
sunstrikelll <sunstrike20021@gmail.com>
2026-02-22 17:13:41 +0300

    try repaeat example (change pin and ch)

--- include/PWM/PWM.h
+++ include/PWM/PWM.h
@@ -5,15 +5,15 @@
 #include "..\..\GD_libraries\CMSIS\DeviceSupport\gd32f10x.h"
 
 #define PWM_PORT           GPIOA
-#define PWM_PIN            GPIO_PIN_0
+#define PWM_PIN            GPIO_PIN_8
 #define PWM_FREQ           GPIO_OSPEED_50MHZ
 #define PWM_MODE           GPIO_MODE_AF_PP
-#define PWM_TIMER          TIMER4
+#define PWM_TIMER          TIMER0
 #define PWM_CHANNEL        TIMER_CH_0
 #define PROCPERIOD         1000
 #define PROCMGFREQ         72
 #define GPIO_CLK           RCU_GPIOA
-#define TIMER_GPIO_CLK     RCU_TIMER4
+#define TIMER_GPIO_CLK     RCU_TIMER0
 #define FOR_TO_PRECENT     100
 
 void pwm_init(void);
--- src/PWM/PWM.c
+++ src/PWM/PWM.c
@@ -8,6 +8,7 @@ void pwm_init(void)
     rcu_periph_clock_enable(TIMER_GPIO_CLK);
     rcu_periph_clock_enable(RCU_AF);
 
+
     timer_deinit(PWM_TIMER);
     timer_oc_parameter_struct timer_ocinitpara;
     timer_parameter_struct timer_initpara;
@@ -28,12 +29,14 @@ void pwm_init(void)
     timer_ocinitpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;
     timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
     
-    timer_channel_output_config(PWM_TIMER, PWM_CHANNEL, &timer_ocinitpara);
 
+
+    timer_channel_output_config(PWM_TIMER, PWM_CHANNEL, &timer_ocinitpara);
+    timer_channel_output_pulse_value_config(PWM_TIMER,PWM_CHANNEL,3999);
     timer_channel_output_mode_config(PWM_TIMER, PWM_CHANNEL, TIMER_OC_MODE_PWM0);
     timer_channel_output_shadow_config(PWM_TIMER, PWM_CHANNEL, TIMER_OC_SHADOW_ENABLE);
 
-    timer_channel_output_pulse_value_config(PWM_TIMER, PWM_CHANNEL, 0);
+    timer_primary_output_config(PWM_TIMER,ENABLE);
 
     timer_auto_reload_shadow_enable(PWM_TIMER);
 
